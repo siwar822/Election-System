a7fbf09041c3e5ca76a42f8fdf51a8c3cb283935
unknown <jradchaima2@gmail.com>
2020-05-11 16:20:27 +0100

    election

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Auth;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+   
+    public function boot()
+    {
+        Blade::if('admin', function () {
+            return Auth::user()->admin;
+        });
+    }
+}
