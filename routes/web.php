0286da021ee5b74744d8f2766e99760ecf986a28
jradchaima <jradchaima2@gmail.com>
2020-05-29 20:07:33 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,42 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
+});
+
+Route::get('/admin', function () {
+    return view('admin.dashbord');
+});
+Route::get('/candidates', function () {
+    return view('admin.candidates');
 });
 
 Auth::routes();
 
 Route::get('/home', 'AppController@home')->name('home')->middleware('auth');
 
-//  Route::resource('/voter', 'VoterController');
- Route::resource('/user', 'UserController')->middleware('auth');
+
+Route::middleware('auth')->group(function () {
+
+
+ 
+
+    Route::group([
+                'middleware' => 'admin',
+                'namespace' => 'Admin',
+                'prefix' => 'admin'
+            ], function () {
+
+                Route::prefix('requests')->group(function () {
+                    Route::get('all', 'demandecontroller@index');
+                    Route::get('waiting', 'demandeController@waiting');
+                    Route::get('approved', 'demandeController@approved');
+                    Route::get('rejected', 'demandeController@rejected');
+                });
+
+          
+Route::resource('/demande','DemandeController');
+Route::get('/demandeac/{id}','DemandeController@accept')->name('accept');
+Route::get('/demanderej/{id}','DemandeController@refuse')->name('refuse');
+            });
+});
