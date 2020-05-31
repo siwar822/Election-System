3880cfa0486324cf0e82c828cf2d284bb09a57c4
jradchaima <jradchaima2@gmail.com>
2020-05-31 11:26:27 +0100

    refuse request

---
+++ app/Mail/RefuseRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class RefuseRequest extends Mailable
+{
+    public $demande;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->demande= $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.refuse');
+    }
+}
